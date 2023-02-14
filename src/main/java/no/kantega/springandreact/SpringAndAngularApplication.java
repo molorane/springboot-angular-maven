2e38909c62bf11fe98e3eb5941c60aad1e55211c
mothusimolorane <molorane91@gmail.com>
2023-02-14 14:42:58 +0200

    first commit

---
+++ src/main/java/no/kantega/springandreact/SpringAndAngularApplication.java
@@ -0,0 +1,12 @@
+package no.kantega.springandreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndAngularApplication.class, args);
+	}
+}
---
+++ src/test/java/no/kantega/springandreact/SpringAndAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package no.kantega.springandreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringAndAngularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
